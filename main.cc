5d082817dd09747faf3c1b0677cbd9d9c5db5479
donatojb <donatojbeneto@gmail.com>
2022-05-16 12:26:51 +0200

    el codi de gerard pero en maco

--- main.cc
+++ main.cc
@@ -5,67 +5,152 @@
 #include <math.h>
 #include <cmath>
 #include <chrono>
-#include "matplotlibcpp.h"
-
-//g++ main.cc -std=c++11 -I/usr/include/python3.8/ -lpython3.8 
-
-//g++ main.cc -std=c++11 -I/home/donato/.local/lib/python3.8/site-packages/numpy/core/include -I/home/donato/.local/lib/python3.8/site-packages/tensorflow/include/external/local_config_python/python_include/ -lpython3.8 -lpthread -lutil -ldl -Xlinker -export-dynamic
 
 using namespace std;
-namespace plt = matplotlibcpp;
 
+typedef vector<int> VE;
 typedef vector<double> V;
 typedef vector<V> VV;
+typedef vector<VV> VVV;
 
 const double kb = 1.3806e-23;
+const double pi = M_PI;
 
-void init_v(double T, double m, V &vx, V &vy) {
+   
+//paramtres de les particules i la caixa
+int Niter = 5000000;
+VE N = {100, 100};
+V m = {6.6335209e-26, 6.6335209e-25};
+VV sig = {{2.576e-10, 2.997e-10}, {2.997e-10, 3.418e-10}};
+VV eps = {{10.2*kb, 34.8*kb}, {34.8*kb, 119*kb}}; 
+double T = 300;
+double lx = 3*1e-8;
+double ly = 3*1e-8;
+double rmax = 10*1e-10;
     
-    default_random_engine generator;
-    generator.seed(chrono::system_clock::now().time_since_epoch().count());
-    double sd = sqrt(kb*T/m);
-    normal_distribution<double> distribution(0.0, sd);
-    for (double &vxi : vx) vxi = distribution(generator);
-    for (double &vyi : vy) vyi = distribution(generator);
+//parametres de simulacio
+double dt = 1e-15;
+VV A = VV(N.size(), V(N.size()));
+VV B = VV(N.size(), V(N.size()));
+
+    
+//vectors de informacio
+VVV x = VVV(N.size());
+VVV y = VVV(N.size());
+VV vx = VV(N.size());
+VV vy = VV(N.size());
+VV fx = VV(N.size());
+VV fy = VV(N.size());
+
+
+
+
+void escriure(V &v, bool salt){
+	for (double vi : v) cout << vi << " ";
+	if (salt) cout << endl;
+}
+
+//a diu si iteracio anterior o actual
+void visualitza(int a) {
+	for (int k = 0; k < (int)N.size(); ++k) 
+		escriure(x[k][a], false);
+	cout << endl;
+	for (int k = 0; k < (int)N.size(); ++k)	
+		escriure(y[k][a], false);
+	cout << endl;
+
+}
+
+
+//part numero indicador del tipus particula
+void init_p() {
+    
+    for (int k = 0; k < N.size(); ++k) {
+        default_random_engine generator;
+        generator.seed(chrono::system_clock::now().time_since_epoch().count());
+        uniform_real_distribution<double> distributionx(0.0, lx);
+        for (double &xi : x[k][1]) xi = distributionx(generator);
+        uniform_real_distribution<double> distributiony(0.0, ly);
+        for (double &yi : y[k][1]) yi = distributiony(generator);
+    }
         
 }
 
-void init_p(double l, V &x, V &y) {
-    
-    default_random_engine generator;
-    generator.seed(chrono::system_clock::now().time_since_epoch().count());
-    uniform_real_distribution<double> distribution(0.0, l);
-    for (double &xi : x) xi = distribution(generator);
-    for (double &yi : y) yi = distribution(generator);
-        
+void init_v() {
+    for (int k = 0; k < N.size(); ++k) {
+        default_random_engine generator;
+        generator.seed(chrono::system_clock::now().time_since_epoch().count());
+        double sd = sqrt(kb*T/m[k]);
+        normal_distribution<double> distribution(0.0, sd);
+        for (double &vxi : vx[k]) vxi = distribution(generator);
+        for (double &vyi : vy[k]) vyi = distribution(generator);
+    }
 }
 
-void force(double m, double A, double B, V &x, V &y, V &fx, V&fy) {
+void init_param() {
 
-    fx = V(fx.size(), 0); 
-    fy = V(fy.size(), 0); 
+    for (int k = 0; k < N.size(); ++k) {
+        x[k] = VV(2, V(N[k]));
+        y[k] = VV(2, V(N[k]));
+        vx[k] = V(N[k]);
+        vy[k] = V(N[k]);
+        fx[k] = V(N[k]);
+        fy[k] = V(N[k]);
+    }
     
-    for (int i = 0; i < (int)x.size(); ++i) 
-        for (int j = 0; j < (int)x.size(); ++j) 
-            if (j != i) {
-                //calculem força de i sobre j
-                double r2 = pow(x[j]-x[i],2) + pow(y[j]-y[i],2);
-                double f = A/pow(r2,7) - B/pow(r2,4);
-                fx[j] += f*(x[j]-x[i]);
-                fy[j] += f*(y[j]-y[i]) - 9.81*m*1e10;
+    for (int k = 0; k < N.size(); ++k)
+        for (int j = 0; j < N.size(); ++j) {
+            A[k][j] = 4*12*eps[k][j]*pow(sig[k][j],12);
+            B[k][j] = 4*6*eps[k][j]*pow(sig[k][j],6);
+    }
+    
+
+}
+
+
+//a diu si utilitzar la x actual o l'anterior
+void force(int a) {
+    
+    for (int k = 0; k < (int)N.size(); ++k) {
+        
+        fx[k] = V(fx[k].size(), 0); 
+        fy[k] = V(fy[k].size(), 0); 
+        
+        for (int h = 0; h < (int)N.size(); ++h) {
+            
+            for (int i = 0; i < N[h]; ++i) {
+                for (int j = 0; j < N[k]; ++j) {
+                    
+                    fy[k][j] += -9.81e10*m[k];
+                    if (j != i or h != k) {
+                        //calculem força de i sobre j- lennard jones
+                        double r2 = pow(x[k][a][j]-x[h][a][i],2) + pow(y[k][a][j]-y[h][a][i],2);
+                        if(r2<rmax*rmax){
+                            double f = A[k][h]/pow(r2,7) - B[k][h]/pow(r2,4);
+                            fx[k][j] += f*(x[k][a][j]-x[h][a][i]);
+                            fy[k][j] += f*(y[k][a][j]-y[h][a][i]);
+                        }
+                    }
+                }
             }
+        }
+    }
 }
 
-void primera_iter(double dt, double m, VV &p, V &v, V &f) {
-    for (int i = 0; i < (int)p[0].size(); ++i) 
-        p[0][i] = p[1][i] + v[i]*dt + 0.5*(f[i]/m)*dt*dt;
+void primera_iter() {
+    
+    force(1);
+    
+    for (int k = 0; k < (int)N.size(); ++k)
+        for (int i = 0; i < N[k]; ++i) {
+            x[k][0][i] = x[k][1][i] + vx[k][i]*dt + 0.5*(fx[k][i]/m[k])*dt*dt;
+            y[k][0][i] = y[k][1][i] + vy[k][i]*dt + 0.5*(fy[k][i]/m[k])*dt*dt;
+        }
 }
 
-void next_iter(double l, double m, double dt, V &f, VV &p, V &v) {
-
+void next_iteri(double l, double m, V &f, VV &p, V &v) {
+   
     for (int i = 0; i < (int)p[0].size(); ++i) {
-        
-        
         
         // formula de l'algorisme de verlett
         double aux = p[0][i];
@@ -81,93 +166,58 @@ void next_iter(double l, double m, double dt, V &f, VV &p, V &v) {
         else if (p[0][i] > l) {
             p[1][i] = 2*l - p[1][i];
             p[0][i] = 2*l - p[0][i];
-        
+        }
         // calcul de la velocitat
         v[i] = (p[0][i] - aux2)/2*dt;
     }
+    
 }
 
-double kineticE (double m, V &vx, V &vy) {
+void next_iter() {
 
-    double E = 0;
-    for (int i = 0; i < (int)vx.size(); ++i) {
-        
-        E += 0.5 * m * (vx[i]*vx[i] + vy[i]*vy[i]);
+    force(0);
+    for (int i = 0; i < (int)N.size(); ++i) {
+        next_iteri(lx, m[i], fx[i], x[i], vx[i]);
+        next_iteri(ly, m[i], fy[i], y[i], vy[i]);
     }
-    return E;
-
 }
+
+
+
+double kineticE () {
+    double E = 0;
+    for (int j = 0; j < N.size(); ++j) {
+        for (int i = 0; i < (int)vx[j].size(); ++i) {
+            
+            E += 0.5 * m[j] * (vx[j][i]*vx[j][i] + vy[j][i]*vy[j][i]);
+        }
+     }
+     return E;
+}
+
+
 
 int main() {
     
-    
-    
-    
-    //paramtres de les particules i la caixa
-    int Niter = 100000;
-    int N = 1000;
-    //double m = 10e-27;
-    double m =   6.6335209e-26;
-    double sig = 2.576e-10;
-     
-    //double sig = 2.576e-10;
-    double sig = 3.405e-10;
-    //double eps = 10.2*kb;
-    double eps = 119.8*kb;
-    double T = 300;
-    double lx = 1e-7;
-    double ly = 1e-7;
-    
-    //parametres de simulacio
-    double dt = 1e-13;
-    double A = 4*12*eps*pow(sig,12);
-    double B = 4*6*eps*pow(sig,6);
-    
-    //vectors de informacio
-    VV x = VV(2, V(N));
-    VV y = VV(2, V(N));
-    V vx = V(N);
-    V vy = V(N);
-    V fx = V(N);
-    V fy = V(N);
+    //inicialitzar parametres
+    init_param();
     
     //inicialitzar velocitats
-    init_v(T, m, vx, vy);
+    init_v();
     
     //inicialitzar posicions
-    init_p(lx, x[1], y[1]);
-    
-    //inicialitzar forces
-    force(m, A, B, x[1], y[1], fx, fy);
+    init_p();
     
     //fer primera iteracio de les posicions sense verlet
-    primera_iter(dt, m, x, vx, fx);
-    primera_iter(dt, m, y, vy, fy);
-       
-       
-    plt::title("Simulació");
-    plt::Plot plot("Heli", "or");
-    plt::legend();
-    plt::xlim(0.0, lx);
-    plt::ylim(0.0, ly);
+    primera_iter();
+    visualitza(1);
     
-    plot.update(x[1], y[1]);
-    plt::pause(0.1);
-    
+	
     for (int k = 0; k < Niter; ++k) {
-        plot.update(x[0], y[0]);
-        plt::pause(0.005);
-        
-        force(m, A, B, x[0], y[0], fx, fy);
-        next_iter(lx, m, dt, fx, x, vx);
-        next_iter(ly, m, dt, fy, y, vy);
-        
-        double E = kineticE(m, vx, vy);
-        cout << E << endl;
+        next_iter();
+        if (k%100 == 0)  visualitza(0);
     }
     
-    
-   
     
 
 }
